522f18a1a2525722b805d519ef6abb559b252c75
Heng Jin En <hengh0305@gmail.com>
2024-01-16 09:31:16 +0800

    my version

--- src/main/java/com/example/database/DatabaseEngine.java
+++ src/main/java/com/example/database/DatabaseEngine.java
@@ -2,6 +2,9 @@ package com.example.database;
 import com.example.database.DataStructure.MyHashMap;
 import com.example.database.DataStructure.MyLinkedList;
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class DatabaseEngine {
@@ -13,31 +16,63 @@ public class DatabaseEngine {
         this.databaseSpine = loadDatabaseFromDisk();
     }
 
-    public void insertData(String dataType,String index, DatabaseItem<?> data) {
-
+    public String insertData(String dataType,String index, DatabaseItem<?> data) {
 
         switch (dataType) {
             case "str":
                 databaseSpine.put(index, new StringItem(data.getValue().toString()));
                 break;
             case "c":
-                databaseSpine.put(index, new CharItem(data.getValue().toString().charAt(0)));
+                if (data.getValue().toString().length() == 1) {
+                    databaseSpine.put(index, new CharItem(data.getValue().toString().charAt(0)));
+                } else {
+                    return "[Database] Error! Character data must be a single character. Please try again!";
+                }
                 break;
             case "num":
                 try {
-                    double numValue = Double.parseDouble(data.getValue().toString());
-                    databaseSpine.put(index, new NumericItem(numValue));
+                    // If it's a whole number, insert as Integer
+                    if (data.getValue().toString().matches("\\d+")) {
+                        int intValue = Integer.parseInt(data.getValue().toString());
+                        databaseSpine.put(index, new NumericItem(intValue));
+                    } else {
+                        // Otherwise, insert as Double
+                        double numValue = Double.parseDouble(data.getValue().toString());
+                        databaseSpine.put(index, new NumericItem(numValue));
+                    }
+
                 } catch (NumberFormatException e) {
-                    e.printStackTrace();
+                    return "[Database] Error! Not a number. Please try again!\n";
                 }
                 break;
+            case "coll":
+                // Check if the data is an array
+                if (data.getValue() instanceof Object[]) {
+                    databaseSpine.put(index, new CollectionItem<>((Object[]) data.getValue()));
+                } else {
+                    return "[Database] Error! Collection data must be an array. Please try again!\n";
+                }
+                break;
+            case "dt":
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // Change the date format as needed
+                    Date dateValue = dateFormat.parse(data.getValue().toString());
+                    databaseSpine.put(index, new DateItem(dateValue));
+                } catch (ParseException e) {
+                    return "[Database] Error! Invalid date format. Please try again!\n";
+                }
+                break;
+            case "bool":
+                boolean boolValue = Boolean.parseBoolean(data.getValue().toString());
+                databaseSpine.put(index, new BooleanItem(boolValue));
+                break;
             default:
+                return "[Database] Unknown data type. Please try again!\n";
 
         }
 
-
+        return null;
     }
-
 
 
     public DatabaseItem<?> getData(String index) {
@@ -48,8 +83,12 @@ public class DatabaseEngine {
         return databaseSpine.remove(index);
     }
 
-    public void clearAllData() {
+    public boolean clearAllData() {
+        if(databaseSpine.isEmpty()){
+            return false;
+        }
         databaseSpine.clear();
+        return true;
     }
 
     public String displayData(String key){
@@ -61,11 +100,21 @@ public class DatabaseEngine {
 
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < keys.size(); i++) {
-            String key = keys.get(i);
-            DatabaseItem<?> item = databaseSpine.get(key);
+        if(databaseSpine.isEmpty()){
+            result.append("\n[Database] Database is empty!\n");
+        }else{
+            result.append("\nDisplay all data:\n");
+            result.append(String.format("%-15s  %-15s  %-15s\n", "index", "type", "value"));
+            for (int i = 0; i < keys.size(); i++) {
+                String key = keys.get(i);
+                DatabaseItem<?> item = databaseSpine.get(key);
 
-            result.append(key).append(" ").append(item.toString()).append("\n");
+                // Append key, type, and value to the result
+                result.append(String.format("%-15s  %-15s  %-15s\n", key, item.getType(), item.toString()));
+
+
+                //result.append(key).append(" ").append(item.toString()).append("\n");
+            }
         }
 
         return result.toString();
